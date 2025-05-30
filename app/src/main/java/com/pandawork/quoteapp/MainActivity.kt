90dab26b7c91ddaf2ee05d016882eff015814713
CodePandaaAI <romitsharmakv@gmail.com>
2025-05-30 23:22:16 +0530

    feat: Basic Quote App Layout
    This commit introduces the basic layout for the Quote App.
    
    Key changes:
    - Added `QuoteScreen` composable which includes a `TopAppBar`, a `Card` for displaying quotes, and "Previous" and "Next" buttons.
    - Implemented `TopAppBar` with a centered title "Quotes".
    - Defined custom fonts: `nunito` for body text and `play_write` for display text.
    - Updated `Typography` to use these custom fonts.
    - Generated and integrated a new color scheme for both light and dark themes.
    - Updated `MainActivity` to display `QuoteScreen`.
    - Added preview for `QuoteScreen`.
    - Included new IDE inspection profiles for Compose.
    - Updated Gradle settings.

--- app/src/main/java/com/pandawork/quoteapp/MainActivity.kt
+++ app/src/main/java/com/pandawork/quoteapp/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.pandawork.quoteapp.ui.theme.QuoteAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +33,70 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             QuoteAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                QuoteScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun QuoteScreen() {
+    Scaffold(
+        topBar = { TopAppBar() }) { innerPadding ->
+        Column(
+            Modifier
+                .padding(horizontal = 16.dp, vertical = 24.dp)
+                .fillMaxSize()
+        ) {
+            Card(
+                Modifier
+                    .height(750.dp)
+                    .padding(innerPadding)
+                    .fillMaxWidth(),
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+
+            ) {
+                Text(
+                    text = "Daily Feed✨",
+                    style = MaterialTheme.typography.titleLarge,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            16.dp
+                        )
+                )
+            }
+            Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+                Button(onClick = {}) {
+                    Text(text = "Previous Quote")
+                }
+                Button(onClick = {}) {
+                    Text(text = "Next Quote")
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar() {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                "Quotes",
+                style = MaterialTheme.typography.displayLarge,
+                fontWeight = FontWeight.Bold
+            )
+        }
     )
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    QuoteAppTheme {
-        Greeting("Android")
+fun QuoteScreenPreview() {
+    QuoteAppTheme(dynamicColor = true) {
+        QuoteScreen()
     }
 }
--- app/src/main/java/com/pandawork/quoteapp/ui/theme/Type.kt
+++ app/src/main/java/com/pandawork/quoteapp/ui/theme/Type.kt
@@ -1,34 +1,50 @@
 package com.pandawork.quoteapp.ui.theme
 
+import android.R.attr.fontFamily
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.pandawork.quoteapp.R
+
+
+val nunito = FontFamily(
+    Font(
+        R.font.nunito_regular,
+        FontWeight.Normal
+    )
+)
+
+val play_write = FontFamily(
+    Font(
+        R.font.play_write_hu_regular,
+        FontWeight.Normal
+    )
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = play_write,
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = nunito,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = nunito,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
